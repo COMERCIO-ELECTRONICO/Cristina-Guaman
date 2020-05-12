357c9f5993bd0bcb5e22e568c7d1af8c551dfb8c
Cristina-248 <crisalexag@hotmail.com>
2020-05-12 17:28:35 -0500

    Typscript

--- typescript/tipo-datos.ts
+++ typescript/tipo-datos.ts
@@ -32,7 +32,7 @@ function sumar(num1?:number, num2?: number): number {
     }
     if(num2){
         b = num2;
-
+    }
     return a+b;
 };
 
@@ -43,8 +43,26 @@ console.log(sumar(5, 5));
 console.log(sumar(5));
 console.log(sumar());
 
+class Persona {
+    constructor(
+        protected nombre?: string,
+        public apellido?: string
+    ) {
 
-    ///los dos parametros sean opcionales
-
-
+    }
 }
+
+const nnuevaPersona = new Persona()
+
+class hijo extends Persona {
+    constructor(
+        protected nombre: string,
+        public apellido: string,
+    ) {
+        super(nombre, apellido);
+        this.apellido = apellido;
+    }
+}
+
+const hijoPersona = new hijo('Cristina', 'Guaman');
+console.log(hijoPersona);
