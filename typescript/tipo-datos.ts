2460906678b5308e7522d41d446d48419b7cdbf9
Cristina-248 <crisalexag@hotmail.com>
2020-05-13 10:43:22 -0500

    get an seder

--- typescript/tipo-datos.ts
+++ typescript/tipo-datos.ts
@@ -66,3 +66,44 @@ class hijo extends Persona {
 
 const hijoPersona = new hijo('Cristina', 'Guaman');
 console.log(hijoPersona);
+
+class Person{
+    constructor(
+        protected nombre?: string,
+        protected apellido?: string
+    ){}
+}
+class Person {
+    constructor(
+        protected nombre?: string,
+        protected apellido?: string
+    ) { }
+}
+
+class alumno extends Person {
+    constructor(
+        protected nombre: string,
+        protected apellido: string
+    ) {
+        super(nombre, apellido);
+    }
+
+    set setNombre(nombre: string | any) {
+        this.nombre = nombre;
+    }
+    set setApellido(apellido: string | any) {
+        this.apellido = apellido;
+    }
+
+    get getNombre(): string | any {
+        return this.nombre
+    }
+    get getApellido(): string | any {
+        return this.nombre
+    }
+}
+
+const alumnoNuevo: alumno = new alumno();
+alumnoNuevo.setNombre('kevin');
+alumnoNuevo.setApellido('');
+alumnoNuevo.getApellido();
