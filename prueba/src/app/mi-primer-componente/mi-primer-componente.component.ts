fe2142e65f664eacedc23f67d9f75e56c3147bf2
Cristina-248 <crisalexag@hotmail.com>
2020-05-16 10:49:19 -0500

    Padre Hijo

--- prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,38 +1,44 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from '@angular/core';
+// @nombreDecorador() decorador
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.css']
 })
 export class MiPrimerComponenteComponent implements OnInit, OnDestroy {
-    @Input() titulo: string;
-    @Input() nombreBoton: string;
-    @Input() imagenPath: string;
-    @Input() datos: object;
 
-    @Output() saludohijo = new EventEmitter();
+  @Input() titulo: string;
+  @Input() nombreBoton: string;
+  @Input() imagenPath: string;
+  @Input() datos: Object;
 
-    fecha = new Date();
-  sueldo: number  = 3.1416;
-  nombre: string = 'Cristina';
+  @Output() saludoHijo = new EventEmitter();
+
+  fecha = new Date();
+  sueldo: number = 3.1416;
+  nombre: string = 'kevin';
+
 
   constructor() { }
 
-  ngOnInit(): void {
-    console.log('datos del padre en hijo')
-    console.log(this.datos)
-  }
-  ngOnDestroy(){
-
+  ngOnInit() {
+    console.log('datos del padre en hijo');
+    console.log(this.datos);
   }
 
+  ngOnDestroy() {
 
-
-
+  }
 
   SALUDAR(){
     alert('Hola :)');
-    this.saludohijo.emit('saludo de tu hijo');
+    this.saludoHijo.emit('saludo de tu hijo');
   }
 }
