a3fcc94e22dfa580c326a1c1da1eb40949ab24a4
Cristina-248 <crisalexag@hotmail.com>
2020-05-16 09:24:47 -0500

    Pipes

--- prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.css']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent implements OnInit, OnDestroy {
     @Input() titulo: string;
     @Input() nombreBoton: string;
     @Input() imagenPath: string;
+    @Input() datos: object;
 
+    @Output() saludohijo = new EventEmitter();
 
+    fecha = new Date();
+  sueldo: number  = 3.1416;
+  nombre: string = 'Cristina';
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('datos del padre en hijo')
+    console.log(this.datos)
   }
+  ngOnDestroy(){
+
+  }
+
+
+
+
+
   SALUDAR(){
     alert('Hola :)');
+    this.saludohijo.emit('saludo de tu hijo');
   }
 }
