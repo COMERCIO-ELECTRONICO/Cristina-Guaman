285600e57f2f2ab6f2be27f1cc291040a5501194
Cristina-248 <crisalexag@hotmail.com>
2020-05-19 18:31:56 -0500

    rutas

--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -1,10 +1,26 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './inicio/home/home.component';
+import { NoEncontradoComponent } from './inicio/no-encontrado/no-encontrado.component';
+import { LoginComponent } from './login/login.component';
 const rutas: Routes = [
     {
         path: 'inicio',
         component: HomeComponent
+    },
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+
+    },
+    {
+        path: '**',
+        component: NoEncontradoComponent
     }
 
 ]
