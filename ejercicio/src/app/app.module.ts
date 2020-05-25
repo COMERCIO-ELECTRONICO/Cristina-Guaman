5abc28580dd82dcc81ebc52ffcd9c9129f24945f
Cristina-248 <crisalexag@hotmail.com>
2020-05-25 08:02:28 -0500

    ejercicio

--- ejercicio/src/app/app.module.ts
+++ ejercicio/src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { AppRoutes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
-import { LoginComponent } from './login/login.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NoEncontradoComponent,
+    InicioSesionComponent,
     LoginComponent,
-    InicioSesionComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutes
-  ],
+  imports: [BrowserModule, AppRoutes, BrowserAnimationsModule, ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ejercicio/src/app/app.routes.ts
+++ ejercicio/src/app/app.routes.ts
@@ -1,27 +1,36 @@
-import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/Router';
 import { HomeComponent } from './home/home.component';
-import { NgModule} from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { LoginComponent } from './login/login.component';
 
-const rutas :Routes = [
-    {
-        path: 'inicio',
-        component: HomeComponent
-    },
-    {
-        path: '',
-        redirectTo: 'inicio',
-        pathMatch: 'full'
-    },
-    {
-        path: '**',
-        component: NoEncontradoComponent
-    }
-]
-
+const rutas: Routes = [
+  {
+    path: 'inicio',
+    component: HomeComponent,
+  },
+  {
+    path: 'iniciar-sesion',
+    component: InicioSesionComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NoEncontradoComponent,
+  },
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(rutas) ],
+  imports: [RouterModule.forRoot(rutas)],
   providers: [],
   exports: [RouterModule],
 })
