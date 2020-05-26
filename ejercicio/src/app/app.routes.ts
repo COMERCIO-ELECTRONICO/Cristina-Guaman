24545dc7485734d150228939ebb7994170f1efa7
Cristina-248 <crisalexag@hotmail.com>
2020-05-25 19:39:19 -0500

    rutas formulario

--- ejercicio/src/app/app.routes.ts
+++ ejercicio/src/app/app.routes.ts
@@ -4,34 +4,39 @@ import { HomeComponent } from './home/home.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { LoginComponent } from './login/login.component';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
 
 const rutas: Routes = [
-  {
-    path: 'inicio',
-    component: HomeComponent,
-  },
-  {
-    path: 'iniciar-sesion',
-    component: InicioSesionComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    component: NoEncontradoComponent,
-  },
+    {
+        path: 'inicio',
+        component: HomeComponent,
+    },
+    {
+        path: 'inicio-sesion',
+        component: InicioSesionComponent,
+    },
+    {
+        path: 'estudiante/perfil',
+        component: PerfilUsuarioComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        component: NoEncontradoComponent,
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(rutas)],
-  providers: [],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(rutas)],
+    providers: [],
+    exports: [RouterModule],
 })
-export class AppRoutes {}
+export class AppRoutes { }
--- ejercicio/src/app/login/login.component.ts
+++ ejercicio/src/app/login/login.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/Router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  // credenciales
 
-  constructor() { }
+  correo = '';
+  pass = '';
+  seleccionadoValor;
 
-  ngOnInit(): void {
+  valorAutocomplete = '';
+  arregloResultado = [];
+  sugerencias = ['Cristina', 'cris', 'Alexandra'];
+
+  constructor(
+    private readonly _router: Router,
+  ) {}
+
+  ngOnInit(): void {}
+
+  buscarSugerencia(evento) {
+    console.log(evento.query);
+    if (evento.query !== '') {
+      const valorEncontrado = this.sugerencias.find((item) => {
+        return item === evento.query;
+      });
+      console.log(valorEncontrado);
+      this.arregloResultado.push(valorEncontrado);
+      this.sugerencias = this.arregloResultado;
+    } else {
+      this.sugerencias = ['Cristina', 'cris', 'Alexandra'];
+    }
   }
 
+  valorSeleccionado(evento) {
+    console.log(evento);
+    this.seleccionadoValor = evento;
+  }
+
+  ingresar() {
+    console.log(this.valorAutocomplete);
+
+    if (this.pass === '1234') {
+      alert(this.correo);
+      if (this.seleccionadoValor === 'Cristina') {
+        alert('es estudiante');
+          this._router.navigate(
+            ['/estudiante','perfil']
+            )
+      }
+    } else {
+      alert('no ingreso');
+    }
+  }
 }
